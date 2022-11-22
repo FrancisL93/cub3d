19682efc38f6a6a71417c8cef8bc11f28a746830
thelord82 <mandrelord@gmail.com>
2022-11-22 11:50:45 -0500

    Work in progress

--- src/map_parsing.c
+++ src/map_parsing.c
@@ -1,3 +1,23 @@
+#include "../include/cub3d.h"
+
 //TODO Verifier les contours 
 //TODO Remplacer les espaces par des 1
 
+void	check_walls(void)
+{
+	t_vars	*vars;
+	int		i;
+	int		j;
+
+	vars = get_data();
+	i = 0;
+	while (vars->map[i])
+	{
+		j = 0;
+		while (vars->map[i][j])
+		{
+			if (vars->map[i][j] == '0' && vars->map[i][j + 1] == ' ')
+				quit_game(13);
+		}
+	}
+}
