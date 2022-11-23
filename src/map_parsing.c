86655fdd659775afd7a7e7b567489dace18645c7
thelord82 <mandrelord@gmail.com>
2022-11-23 14:25:00 -0500

    Parsing in progress

--- src/map_parsing.c
+++ src/map_parsing.c
@@ -1,7 +1,102 @@
 #include "../include/cub3d.h"
 
 //TODO Verifier les contours 
-//TODO Remplacer les espaces par des 1
+//TODO Fonction check_walls un coup nouvelle carte générée
+
+void	check_walls(void)
+{
+	//REFACTOR for Norminette
+	
+	t_vars	*vars;
+	int		line;
+	int		column;
+
+	vars = get_data();
+	line = 0;
+	column = 0;
+	while (vars->map[line])
+	{
+		if (line == 0)
+		{
+			while (vars->map[0][column] && column < (int)ft_strlen(vars->map[0]) - 1)
+			{
+				if (vars->map[0][column] != '1')
+					quit_game(13);
+				column++;
+			}
+			column = 0;
+			line++;
+		}
+		if (vars->map[line][0] != '1' || vars->map[line][ft_strlen(vars->map[line]) - 2] != '1')
+			quit_game(13);
+		if (!vars->map[line + 1])
+		{
+			column = 0;
+			while (vars->map[line][column] && column < (int)ft_strlen(vars->map[line]))
+			{
+				if (vars->map[line][column] != '1')
+					quit_game(13);
+				column++;
+			}
+		}
+		line++;
+	}
+	printf("MAP EST OK\n");
+}
+
+void	check_spaces(int line, int column)
+{
+	t_vars	*vars;
+
+	vars = get_data();
+	if ((column >= 0 && vars->map[line][column + 1]
+		&& vars->map[line][column + 1] == '0')
+		|| (column >= 0 && vars->map[line][column - 1]
+		&& vars->map[line][column - 1] == '0')
+			|| (line >= 0 && vars->map[line + 1]
+			&& vars->map[line + 1][column] == '0')
+				|| (line > 0 && vars->map[line - 1][column]
+				&& vars->map[line - 1][column] == '0'))
+		quit_game(13);
+	else
+		vars->map[line][column] = '1';
+}
+
+static void	print_new_map(int i, int j)
+{
+	t_vars	*vars;
+
+	vars = get_data();
+	while (vars->map[i])
+	{
+		j = 0;
+		while (vars->map[i][j])
+		{
+			printf("%c", vars->map[i][j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+//TODO Verifier ce qui est tout autour des zeros
+void	check_zeros(int line, int column)
+{
+	t_vars	*vars;
+
+	vars = get_data();
+	if ((column >= 0 && vars->map[line][column + 1]
+		&& vars->map[line][column + 1] == '0')
+		|| (column >= 0 && vars->map[line][column - 1]
+		&& vars->map[line][column - 1] == '0')
+			|| (line >= 0 && vars->map[line + 1]
+			&& vars->map[line + 1][column] == '0')
+				|| (line > 0 && vars->map[line - 1][column]
+				&& vars->map[line - 1][column] == '0'))
+		quit_game(13);
+	else
+		vars->map[line][column] = '1';
+}
 
 void	spaces_handling(void)
 {
@@ -17,19 +112,7 @@ void	spaces_handling(void)
 		while (vars->map[i][j])
 		{
 			if (vars->map[i][j] == ' ')
-			{
-				if ((j >= 0 && vars->map[i][j + 1]
-					&& vars->map[i][j + 1] == '0')
-					|| (j >= 0 && vars->map[i][j - 1]
-					&& vars->map[i][j - 1] == '0')
-						|| (i >= 0 && vars->map[i + 1][j]
-						&& vars->map[i + 1][j] == '0')
-							|| (i > 0 && vars->map[i - 1][j]
-							&& vars->map[i - 1][j] == '0'))
-					quit_game(13);
-				else
-					vars->map[i][j] = '1';
-			}
+				check_spaces(i, j);
 			j++;
 		}
 		i++;
@@ -37,14 +120,6 @@ void	spaces_handling(void)
 	i = 0;
 	j = 0;
 	printf("NOUVELLE MAP ------------------\n");
-	while (vars->map[i])
-	{
-		j = 0;
-		while (vars->map[i][j])
-		{
-			printf("%c", vars->map[i][j]);
-			j++;
-		}
-		i++;
-	}
+	print_new_map(i, j);
+	check_walls();
 }
