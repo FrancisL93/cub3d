7a3b08b35610065ec6d16410c25bb2684ca8ff5b
Marc-Andre <mandrelord@gmail.com>
2022-11-29 16:02:34 -0500

    Fixed start_point and semi segfault

--- src/map_tools.c
+++ src/map_tools.c
@@ -6,13 +6,13 @@ void	set_direction(char dir)
 
 	vars = get_data();
 	if (dir == 'N')
-		vars->game->dirx = 180;
-	else if (dir == 'E')
-		vars->game->dirx = 90;
-	else if (dir == 'S')
-		vars->game->dirx = 0;
-	else if (dir == 'W')
 		vars->game->dirx = 270;
+	else if (dir == 'E')
+		vars->game->dirx = 0;
+	else if (dir == 'S')
+		vars->game->dirx = 90;
+	else if (dir == 'W')
+		vars->game->dirx = 180;
 }
 
 void	set_up_start(void)
