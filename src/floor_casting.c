f3c1a47af4ded3b931464a21226d69b8e6d11b9f
thelord82 <mandrelord@gmail.com>
2022-12-08 16:25:05 -0500

    Floor and ceiling WORKS !!!!

--- src/floor_casting.c
+++ src/floor_casting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   floor_casting.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flahoud <flahoud@student.42.fr>            +#+  +:+       +#+        */
+/*   By: malord <malord@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 12:51:18 by flahoud           #+#    #+#             */
-/*   Updated: 2022/12/08 13:42:05 by flahoud          ###   ########.fr       */
+/*   Updated: 2022/12/08 15:57:41 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,14 @@ void	put_ceiling_pixel(int y, int x, double tile_x, double tile_y)
 {
 	char	*tmp;
 	char	*text_tmp;
-	int		texture_x;
-	int		texture_y;
 	t_vars	*vars;
 
 	vars = get_data();
 	tmp = init_tmp(x, &y);
-	texture_x = (int) floor(tile_x * vars->img->ceiling_size[0]) % vars->img->ceiling_size[0];
-	texture_y = (int) floor(tile_y * vars->img->ceiling_size[1]) % vars->img->ceiling_size[1];
-	text_tmp = get_texture_pixel(vars->img->ceiling, texture_x, texture_y);
+	tile_x = abs((int) tile_x % vars->img->ceiling_size[0]);
+	tile_y = abs((int) tile_y % vars->img->ceiling_size[1]);
+	//tile_y = (int) floor(tile_y * vars->img->ceiling_size[1]) % vars->img->ceiling_size[1];
+	text_tmp = get_texture_pixel(vars->img->ceiling, tile_x, tile_y);
 	*tmp++ = *text_tmp++;
 	*tmp++ = *text_tmp++;
 	*tmp++ = *text_tmp++;
@@ -32,17 +31,15 @@ void	put_ceiling_pixel(int y, int x, double tile_x, double tile_y)
 
 void	put_floor_pixel(int y, int x, double tile_x, double tile_y)
 {
-	int		texture_x;
-	int		texture_y;
 	char	*tmp;
 	char	*text_tmp;
 	t_vars	*vars;
 
 	vars = get_data();
 	tmp = init_tmp(x, &y);
-	texture_x = (int) floor(tile_x * vars->img->floor_size[0] / vars->img->floor_size[0]);
-	texture_y = (int) floor(tile_y * vars->img->floor_size[1] / vars->img->floor_size[1]);
-	text_tmp = get_texture_pixel(vars->img->floor, texture_x, texture_y);
+	tile_x = abs((int) tile_x % vars->img->floor_size[0]);
+	tile_y = abs((int) tile_y % vars->img->floor_size[1]);
+	text_tmp = get_texture_pixel(vars->img->floor, tile_x, tile_y);
 	*tmp++ = *text_tmp++;
 	*tmp++ = *text_tmp++;
 	*tmp++ = *text_tmp++;
@@ -66,8 +63,8 @@ void	floor_casting(int y, int x, double *directions, double angle)
 	d = straight_line_dist / cos(angle_beta * (PI / 180));
 	floor_xy[0] = vars->game->posx + cos(angle * (PI / 180)) * d;
 	floor_xy[1] = vars->game->posy - sin(angle * (PI / 180)) * d;
-	//printf("%f & %f |", floor_xy[0], floor_xy[1]);
-	put_floor_pixel(y, x, -floor_xy[0], floor_xy[1]);
+	put_floor_pixel(y, x, floor_xy[0], floor_xy[1]);
+	put_ceiling_pixel(vars->win_height - y, x, floor_xy[0], floor_xy[1]);
 	(void) directions;
 	(void) x;
 }
